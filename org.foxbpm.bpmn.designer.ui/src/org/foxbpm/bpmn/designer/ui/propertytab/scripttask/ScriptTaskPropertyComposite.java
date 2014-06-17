86ce8fd3534f98192bebb4d841275936df059945
demonrain <demonrain@vip.qq.com>
2014-06-17 21:43:57 +0800

    脚本任务完成

--- org.foxbpm.bpmn.designer.ui/src/org/foxbpm/bpmn/designer/ui/propertytab/scripttask/ScriptTaskPropertyComposite.java
+++ org.foxbpm.bpmn.designer.ui/src/org/foxbpm/bpmn/designer/ui/propertytab/scripttask/ScriptTaskPropertyComposite.java
@@ -1,21 +1,33 @@
 package org.foxbpm.bpmn.designer.ui.propertytab.scripttask;
 
 import org.eclipse.bpmn2.Bpmn2Package;
+import org.eclipse.bpmn2.FormalExpression;
 import org.eclipse.bpmn2.ScriptTask;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.transaction.RecordingCommand;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
+import org.foxbpm.bpmn.designer.base.utils.FoxBPMDesignerUtil;
 import org.foxbpm.bpmn.designer.core.runtime.AbstractFoxBPMComposite;
+import org.foxbpm.bpmn.designer.ui.expdialog.ExpressionChangedEvent;
+import org.foxbpm.bpmn.designer.ui.expdialog.FoxBPMExpViewer;
+import org.foxbpm.bpmn.designer.ui.expdialog.IExpressionChangedListener;
+import org.foxbpm.model.bpmn.foxbpm.Expression;
+import org.foxbpm.model.bpmn.foxbpm.FoxBPMFactory;
+import org.foxbpm.model.bpmn.foxbpm.FoxBPMPackage;
 
 public class ScriptTaskPropertyComposite extends AbstractFoxBPMComposite{
 	private ScriptTask scriptTask;
 	private Text descText;
 	private Text idText;
 	private Text nameText;
+	private FoxBPMExpViewer ExpViewer;
 
 	public ScriptTaskPropertyComposite(Composite parent, int style) {
 		super(parent, style);
@@ -27,6 +39,64 @@ public class ScriptTaskPropertyComposite extends AbstractFoxBPMComposite{
 		bindText(Bpmn2Package.Literals.BASE_ELEMENT__ID, idText, scriptTask);
 		bindText(Bpmn2Package.Literals.FLOW_ELEMENT__NAME, nameText, scriptTask);
 		bindDocumentation(Bpmn2Package.Literals.BASE_ELEMENT__DOCUMENTATION, descText);
+		
+		String name = null;
+		Expression expression = null;
+		if (scriptTask.getScript() != null) {
+			expression = FoxBPMFactory.eINSTANCE.createExpression();
+
+			name = scriptTask.eGet(FoxBPMPackage.Literals.DOCUMENT_ROOT__SCRIPT_NAME).toString();
+
+			if (name != null) {
+				expression.setName(name);
+			}
+
+			expression.setValue(scriptTask.getScript());
+		}
+		
+		if (expression == null) {
+			expression = FoxBPMFactory.eINSTANCE.createExpression();
+			expression.setName("");
+			expression.setValue("");
+		}
+		
+		ExpViewer.getTextControl().setText(name==null? "": name);
+		ExpViewer.setExpression(expression);
+		ExpViewer.seteObject(eObject);
+
+		ExpViewer.addExpressionChangedListeners(new IExpressionChangedListener() {
+
+			@Override
+			public void expressionChanged(final ExpressionChangedEvent event) {
+				TransactionalEditingDomain editingDomain = getDiagramEditor().getEditingDomain();
+				editingDomain.getCommandStack().execute(new RecordingCommand(editingDomain) {
+					@Override
+					protected void doExecute() {
+
+						if (event.getFormalExpression() != null) {
+							getBusinessObject().eSet(FoxBPMPackage.Literals.DOCUMENT_ROOT__SCRIPT_NAME, event.getFormalExpression().eGet(FoxBPMPackage.Literals.DOCUMENT_ROOT__NAME).toString());
+							scriptTask.setScriptFormat(FoxBPMDesignerUtil.getProperties().get("ScriptFormat").toString());
+							scriptTask.setScript(event.getFormalExpression().getBody());
+						} else {
+							scriptTask.setScriptFormat(null);
+							getBusinessObject().eSet(FoxBPMPackage.Literals.DOCUMENT_ROOT__SCRIPT_NAME, null);
+							scriptTask.setScript(null);
+						}
+
+						FormalExpression formalExpression = event.getFormalExpression();
+						Expression expression = FoxBPMFactory.eINSTANCE.createExpression();
+						if(formalExpression!=null) {
+							Object expName = formalExpression.eGet(FoxBPMPackage.Literals.DOCUMENT_ROOT__NAME);
+							expression.setName(expName==null?"":expName.toString());
+							expression.setValue(formalExpression.getBody());
+						}
+						//传递表达式对象
+						ExpViewer.setExpression(expression);
+					}
+				});
+
+			}
+		});
 	}
 
 	@Override
@@ -48,6 +118,13 @@ public class ScriptTaskPropertyComposite extends AbstractFoxBPMComposite{
 
 		nameText = new Text(detailComposite, SWT.BORDER);
 		nameText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Label expLabel = new Label(detailComposite, SWT.NONE);
+		expLabel.setText("表达式");
+		
+		ExpViewer = new FoxBPMExpViewer(detailComposite, SWT.BORDER);
+		Control control = ExpViewer.getControl();
+		control.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
 		
 		Label descLabel = new Label(detailComposite, SWT.NONE);
 		descLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
