de770c5346fbcef482f37f8e237a5a35794a6fd3
maenliang <1154507095@qq.com>
2014-07-30 17:03:31 +0800

    流程定义新规的时候添加选择模版功能

--- org.foxbpm.bpmn.designer.ui/src/org/foxbpm/bpmn/designer/ui/wizard/FoxFlowNewWizard.java
+++ org.foxbpm.bpmn.designer.ui/src/org/foxbpm/bpmn/designer/ui/wizard/FoxFlowNewWizard.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright 1996-2014 FoxBPM ORG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @author MAENLIANG
+ */
 package org.foxbpm.bpmn.designer.ui.wizard;
 
 import java.io.BufferedInputStream;
@@ -16,8 +33,10 @@ import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.ISelection;
@@ -32,26 +51,39 @@ import org.eclipse.ui.ide.IDE;
 import org.foxbpm.bpmn.designer.base.utils.FoxBPMDesignerUtil;
 import org.foxbpm.bpmn.designer.ui.utils.BpmnModelUtil;
 
+/**
+ * 新建流程定义对话框
+ * 
+ * @author MAENLIANG
+ * 
+ */
 public class FoxFlowNewWizard extends Wizard implements INewWizard {
 	private ISelection selection;
+	/**
+	 * 流程定义名称输入
+	 */
 	private NewProcessWizardPage newProcessWizardPage;
-
+	/**
+	 * 流程定义模版选择
+	 */
+	private SelectProcessTemplateWizardPage selectProcessTemplateWizardPage;
+	
 	public FoxFlowNewWizard() {
 		setNeedsProgressMonitor(true);
 	}
-
+	
 	@Override
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
 		this.selection = selection;
 	}
-
+	
 	@Override
 	public boolean performFinish() {
 		final String fileName = newProcessWizardPage.getProcessIdText();
 		final String processName = newProcessWizardPage.getProcessNameText();
 		final IResource container = newProcessWizardPage.getDiagramContainer();
 		final String targetNamespace = "";
-
+		
 		IRunnableWithProgress op = new IRunnableWithProgress() {
 			@Override
 			public void run(IProgressMonitor monitor) throws InvocationTargetException {
@@ -62,7 +94,8 @@ public class FoxFlowNewWizard extends Wizard implements INewWizard {
 					IContainer container = (IContainer) resource;
 					final IFile file = container.getFile(new Path(fileName));
 					try {
-						String iofilePath = FoxBPMDesignerUtil.getTempleteFilePath();
+						// 获取模板文件路径
+						String iofilePath = selectProcessTemplateWizardPage.getModelPath();
 						File iofile = new File(iofilePath);
 						InputStream stream = new BufferedInputStream(new FileInputStream(iofile));
 						if (file.exists()) {
@@ -105,20 +138,77 @@ public class FoxFlowNewWizard extends Wizard implements INewWizard {
 		BPMN2MultiPageEditor bpmn2MultiPageEditor = (BPMN2MultiPageEditor) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
 		BPMN2Editor bpmn2Editor = bpmn2MultiPageEditor.getDesignEditor();
 		BpmnModelUtil.updateBpmnModelFile(bpmn2Editor, fileName.substring(0, fileName.lastIndexOf(".")), processName);
-//		try {
-			bpmn2MultiPageEditor.doSave(new NullProgressMonitor());
-//			bpmn2Editor.getDiagramBehavior().getModelHandler().getResource().save(null);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
+		// try {
+		bpmn2MultiPageEditor.doSave(new NullProgressMonitor());
+		// bpmn2Editor.getDiagramBehavior().getModelHandler().getResource().save(null);
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
 		
 		return true;
 	}
-
+	
+	private void doFinish(String containerName, String fileName, IProgressMonitor monitor)
+	    throws CoreException {
+		// create a sample file
+		monitor.beginTask("Creating " + fileName, 2);
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		IResource resource = root.findMember(new Path(containerName));
+		if (!resource.exists() || !(resource instanceof IContainer)) {
+			throwCoreException("Container \"" + containerName + "\" does not exist.");
+		}
+		IContainer container = (IContainer) resource;
+		final IFile file = container.getFile(new Path(fileName));
+		try {
+			InputStream stream = openContentStream();
+			if (file.exists()) {
+				file.setContents(stream, true, true, monitor);
+			} else {
+				file.create(stream, true, monitor);
+			}
+			if (stream != null)
+				stream.close();
+		} catch (IOException e) {
+		}
+		
+		monitor.worked(1);
+		monitor.setTaskName("Opening file for editing...");
+		getShell().getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+				try {
+					IDE.openEditor(page, file, true);
+				} catch (PartInitException e) {
+				}
+			}
+		});
+		monitor.worked(1);
+	}
+	
+	private void throwCoreException(String message) throws CoreException {
+		IStatus status = new Status(IStatus.ERROR, "com.founder.fix.fixflow.designer.fixflow", IStatus.OK, message, null);
+		throw new CoreException(status);
+	}
+	
+	private InputStream openContentStream() {
+		String fakepath = "";
+		InputStream inputStream = null;
+		try {
+			fakepath = selectProcessTemplateWizardPage.getModelPath();
+			File file = new File(fakepath);
+			inputStream = new BufferedInputStream(new FileInputStream(file));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return inputStream;
+		
+	}
+	
 	@Override
 	public void addPages() {
+		selectProcessTemplateWizardPage = new SelectProcessTemplateWizardPage(selection);
+		this.addPage(selectProcessTemplateWizardPage);
 		newProcessWizardPage = new NewProcessWizardPage(selection);
 		this.addPage(newProcessWizardPage);
 	}
-
 }
